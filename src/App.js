4af25a02b440063438fdf2c72438a76f0e956784
nowfdev <nowf.dev@gmail.com>
2024-09-15 16:21:45 +0700

    Create product context

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import Home from "./routes/home/home.component";
 import Authentication from "./routes/authentication/authentication.component";
 import Navigation from "./routes/navigation/navigation.component";
 import { Routes, Route } from "react-router-dom";
+import Shop from "./routes/shop/shop.component";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigation></Navigation>}>
         <Route index element={<Home></Home>}></Route>
+        <Route path="shop" element={<Shop></Shop>}></Route>
         <Route path="auth" element={<Authentication></Authentication>}></Route>
       </Route>
     </Routes>
