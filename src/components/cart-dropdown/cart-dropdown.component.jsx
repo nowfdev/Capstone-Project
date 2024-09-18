1d78dd4bdbc82f0033a30eed126ce50a7f9f7878
nowfdev <nowf.dev@gmail.com>
2024-09-19 01:42:40 +0700

    Implement feature addToCart & Update UI Cart Item

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,10 +1,18 @@
 import Button from "../button/button.component";
 import "./cart-dropdown.styles.scss";
+import CartItem from "../cart-item/cart-item.component";
+import { CardContext } from "../../contexts/cart-context";
+import { useContext } from "react";
 
 const CardDropdown = () => {
+  const { cartItems } = useContext(CardContext);
   return (
     <div className="cart-dropdown-container">
-      <div className="cart-item"></div>
+      <div className="cart-items">
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item}></CartItem>
+        ))}
+      </div>
       <Button>Check out</Button>
     </div>
   );
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,15 +1,16 @@
-import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
-import "./cart-icon.styles.scss";
 import { useContext } from "react";
+import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import { CardContext } from "../../contexts/cart-context";
 
+import "./cart-icon.styles.scss";
+
 const CartIcon = () => {
-  const { isCartOpen, setIsCartOpen } = useContext(CardContext);
+  const { isCartOpen, setIsCartOpen, cartCount } = useContext(CardContext);
   const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
   return (
     <div className="cart-icon-container" onClick={toggleIsCartOpen}>
       <ShoppingIcon className="shopping-icon"></ShoppingIcon>
-      <span className="item-count">0</span>
+      <span className="item-count">{cartCount}</span>
     </div>
   );
 };
