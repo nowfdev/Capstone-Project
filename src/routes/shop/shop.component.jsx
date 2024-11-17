9405a971b449575124749eede1dba426fd7a2b39
nowfdev <nowf.dev@gmail.com>
2024-11-17 16:20:13 +0700

    Update Category page

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,24 +1,14 @@
-import { useContext } from "react";
-import { CategoriesContext } from "../../contexts/categories.context";
-
-import CategoryPreview from "../../components/category-preview/category-preview.component";
+import { Route, Routes } from "react-router-dom";
+import CategoriesPreview from "../categories-preview/categories-preview.component";
+import Category from "../category/category.component";
 
 import "./shop.styles.scss";
 const Shop = () => {
-  const { categoriesMap } = useContext(CategoriesContext);
   return (
-    <div className="shop-container">
-      {Object.keys(categoriesMap).map((title) => {
-        const products = categoriesMap[title];
-        return (
-          <CategoryPreview
-            key={title}
-            title={title}
-            products={products}
-          ></CategoryPreview>
-        );
-      })}
-    </div>
+    <Routes>
+      <Route index element={<CategoriesPreview />}></Route>
+      <Route index path=":category" element={<Category />}></Route>
+    </Routes>
   );
 };
 
